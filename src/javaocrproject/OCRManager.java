ef848100cdf0d5796739c9b4776ffdd292d4b9f6
Xiang <Xiang@172.26.193.60>
2025-05-07 15:30:13 +0800

    基础界面，待完善

--- src/javaocrproject/OCRManager.java
+++ src/javaocrproject/OCRManager.java
@@ -9,5 +9,22 @@ package javaocrproject;
  * @author Xiang
  */
 public class OCRManager {
-    
+     public static String recognize(String imagePath, String engineName) {
+        OCRLibrary ocr;
+
+        switch (engineName.toLowerCase()) {
+            case "mock":
+                ocr = new MockOCR();
+                break;
+
+            case "tesseract":
+                ocr = new TesseractOCR();
+                break;
+
+            default:
+                return "错误：未知识别引擎 \"" + engineName + "\"";
+        }
+
+        return ocr.recognize(imagePath);
+    }
 }
